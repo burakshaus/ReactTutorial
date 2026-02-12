472cf34ce43c4885228187aae97189943f50d65c
Burak <ahmetcanburak54@gmail.com>
2026-02-12 15:29:54 +0300

    continue

--- w3/my-react-app/src/App.jsx
+++ w3/my-react-app/src/App.jsx
@@ -1,9 +1,51 @@
-function App(){
+import { useEffect } from "react";
+
+function App() {
+  useEffect(() => {
+    const myHeader = new Header();
+    const btn = document.getElementById("btn");
+    if(btn) {
+      btn.addEventListener("click", myHeader.changeColor);
+    }
+  }, []);
+  const mycar = new Model("BMW","Mustang");
+  //mycar.present();
+  mycar.show();
   return (
-    <div className="App">
-      <h1>Hello World!</h1>
-    </div>
+    <h1>Hello World!</h1>
   );
 }
+class Car{
+  constructor(name){
+    this.brand = name;
+  }
+  present(){
+    return "I have a " + this.brand;
+  }
+}
 
+class Model extends Car {
+  constructor(name,mod){
+    super(name);
+    this.model=mod;
+  }
+  show(){
+    return this.present() + ", it is a " + this.model;
+  }
+}
+
+
+
+const hello = () => 
+  "Hello World!";
+
+const hello2 = (val) => "Hello " + val;
+class Header {
+  constructor(){
+    this.color = "Red";
+  }
+  changeColor = () =>{
+    document.getElementById("demo").innerHTML += this.color;
+  }
+}
 export default App;
